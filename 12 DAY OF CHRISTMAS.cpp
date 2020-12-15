6b00164154405f3fa262d27d1d1c35f8cee75c3d
Drei <61304544+Andreizabala@users.noreply.github.com>
2020-12-15 12:48:10 +0800

    Add files via upload

---
+++ 12 DAY OF CHRISTMAS.cpp
@@ -0,0 +1,122 @@
+#include<toolkit.h>
+
+
+main(){
+	
+	int day;
+	
+	for(day=1;day<=12;day++)
+	
+	{
+		
+		getch();
+		
+		switch (day){
+	
+		
+		case 1:
+			printf("On the %dst day of christmas my true love sent to me\n",day);
+			break;
+		
+		
+		case 2:
+			printf("\nOn the %dnd day of christmas my true love sent to me\n",day);
+			break;
+		
+	
+		case 3:
+			printf("\nOn the %drd day of christmas my true love sent to me\n",day);
+			break;
+		
+		
+		case 4:
+			case 5:
+				case 6:
+					case 7:
+						case 8:
+							case 9:
+								case 10:
+									case 11:
+										case 12:
+			printf("\nOn the %dth day of christmas my true love sent to me\n",day);
+			break;
+			default:printf("ERROR");
+			
+		
+			}
+			
+	getche();
+			
+		
+			if(day>=12 && day<= 12){
+				printf("Twelve drummers drumming\n");
+				getch();
+			
+			}
+			if(day>=11 && day<= 12){
+				printf("Eleven pipers piping\n");
+				getch();
+				
+			
+			}
+			if(day>=10 && day<= 12){
+				printf("Ten lords a leaping\n");
+				getch();
+			
+			}
+			if(day>=9 && day<= 12){
+				printf("Nine ladies dancing\n");
+				getch();
+			
+			}
+			if(day>=8 && day<= 12){
+				printf("Eight maids a milking\n");
+				getch();
+			
+			}
+			
+			if(day>=7 && day<= 12){
+				printf("Seven swans a swimming\n");
+				getch();
+			
+			}
+			
+			if(day>=6 && day<= 12){
+				printf("Six geese a laying\n");
+				getch();
+			
+			}
+			if(day>=5 && day<= 12){
+				printf("Five golden rings\n");
+				getch();
+			
+			}
+			if(day>=4 && day<= 12){
+				printf("Four calling birds\n");
+				getch();
+			
+			}
+			
+			if(day>=3 && day<= 12){
+				printf("Three French Hens\n");
+				getch();
+			
+			}
+			
+			if(day>=2 && day<= 12){
+				printf("Two turtle doves\n");
+				getch();
+				printf("And a Patridge in a Pear Tree\n");
+		
+			
+			
+			}
+			
+			 if(day==1){
+				printf("A Patridge in a Pear Tree\n");
+					
+		}
+		
+	
+	}
+}
